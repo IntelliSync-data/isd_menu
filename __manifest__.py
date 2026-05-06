30662acf60e988a7c087eb9eca8ed1c9d385f9b9
Huan Nguyen <huan.nguyen@intellisyncdata.com>
2026-05-06 22:45:42 +0700

    Revert 19 to 18

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'ISD Menu Manager',
-    'version': '19.0.0.0.0',
+    'version': '18.0.1.0.0',
     'category': 'ISD Modules',
     'summary': 'Custom menu visibility management per user',
     'description': """
