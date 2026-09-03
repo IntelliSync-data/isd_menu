af99f7a5a1a4f210320bc3f2d4cc9f4d539356e2
Huan Nguyen <huan.nguyen@intellisyncdata.com>
2026-09-03 22:07:25 +0700

    feat: translate menu

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'ISD Menu Manager',
+    'name': 'Menu Manager (by ISD)',
     'version': '18.0.1.0.0',
     'category': 'ISD Modules',
     'summary': 'Custom menu visibility management per user',
